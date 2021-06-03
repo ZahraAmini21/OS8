f65966ef0ddd18da8bbd2944d1fb35e6166a3ec3
Zahra <zahra.amini261999@gmail.com>
2021-06-03 13:23:03 +0430

    modified

---
+++ OS8-1.sh
@@ -0,0 +1,11 @@
+ping 4.2.2.4
+mkdir file1
+mkdir sample
+mkdir test
+chmod 389 file1
+chmod 440 file1
+chmod a=rx file1
+chmod g=w sample
+chmod r+x sample
+chmod u+g test
+
---
+++ OS8-2.sh
@@ -0,0 +1,4 @@
+ping 4.2.2.4
+chmod -R
+chmod -c
+chmod -v
